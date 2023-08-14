e311c09412c3dce3d08b497cbb977b883330f045
Ian Kulin <IanKulin@kulin.com.au>
2023-08-14 12:56:03 +0000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/IanKulin/vitals-glimpse
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"syscall"
+)
+
+func main() {
+	fmt.Printf("Percent Used Memory: %d \n", percentMemUsed())
+	fmt.Printf("Percent Used Disk: %d \n", percentDiskUsed())
+}
+
+
+func parseInt(s string) int {
+	var value int
+	fmt.Sscanf(s, "%d", &value)
+	return value
+}
+
+
+func percentMemUsed() int {
+	memInfo, err := os.ReadFile("/proc/meminfo")
+	if err != nil {
+		log.Println("Error reading /proc/meminfo:", err)
+		return -1
+	}
+
+	memInfoLines := strings.Split(string(memInfo), "\n")
+	memStats := make(map[string]int)
+
+	for _, line := range memInfoLines {
+			parts := strings.Fields(line)
+			if len(parts) >= 2 {
+				key := parts[0]
+				value := parts[1]
+				memStats[key] = parseInt(value)
+			}
+	}
+
+	percentAvail := float32(memStats["MemAvailable:"])*100/(float32(memStats["MemTotal:"]))
+	return 100-int(percentAvail)
+}
+
+
+func percentDiskUsed() int {
+	var stat syscall.Statfs_t
+
+	err := syscall.Statfs("/", &stat)
+	if err != nil {
+		log.Println("Error fetching Statfs for '/'", err)
+		return -1
+	}
+
+	totalBlocks := stat.Blocks
+	availableBlocks := stat.Bavail
+
+	var totalSpace = int(totalBlocks)
+	if totalSpace == 0 {
+		log.Println("totalSpace unexpectedly zero")
+		totalSpace = -1
+	}
+	availableSpace := int(availableBlocks)
+
+	return 100-int(availableSpace*100/totalSpace)
+}
